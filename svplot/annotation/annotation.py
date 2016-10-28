9499e0020208d7a3343544b308b9c84cd23d7fe6
Matthew Stone <matthew.stone12@gmail.com>
2016-10-28 11:47:24 -0400

    fix position transformation

--- svplot/annotation/annotation.py
+++ svplot/annotation/annotation.py
@@ -9,10 +9,12 @@
 """
 
 import numpy as np
+import itertools as it
 
 
 def add_count_label(ax, count, pct=False, as_pct=True, horiz=False,
-                    orient='v', loc='above',
+                    orient='v', loc='above', offset=0.01,
+                    color='black', palette=None,
                     fontsize=11):
     """
     Add count labels to a bar or count plot.
@@ -28,6 +30,8 @@ def add_count_label(ax, count, pct=False, as_pct=True, horiz=False,
         - above: Labels will be plotted above the top each bar
         - inside: Labels will be plotted inside each bar, at the top
         - base: Labels will be plotted at the base of each bar
+    offset : float, optional
+        Offset of the label relative to the bar end. Scaled to a [0, 1] axis.
     color : matplotlib color, optional
         Text color.
     palette : list of matplotlib colors or seaborn color palette, optional
@@ -48,8 +52,14 @@ def add_count_label(ax, count, pct=False, as_pct=True, horiz=False,
         ha = 'center'
     else:
         ha = 'left'
+    if loc == 'above' or loc == 'base':
+        va = 'bottom'
+    else:
+        va = 'top'
     xmin, xmax = ax.get_xlim()
     ymin, ymax = ax.get_ylim()
+    x_width = xmax - xmin
+    y_height = ymax - ymin
 
     # Compute position of label relative to patch size
     # Height for vertical bars, width for horizontal bars
@@ -67,10 +77,14 @@ def add_count_label(ax, count, pct=False, as_pct=True, horiz=False,
 
     print(ax.get_xlim())
 
+    # position relative to bar size (y for vertical, x for horizontal)
+    # is transformed by axis. The position relative to categorical axis is
+    # calculated without need for transformation, so we need to reverse
+    # transform it before passing it to matplotlib
     def _reverse_transform(pos, ax_min, ax_max):
         return (pos - ax_min) / (ax_max - ax_min)
 
-    def _xpos(patch, max_height):
+    def _xpos(patch):
         if orient == 'v':
             x = patch.get_x()
             width = patch.get_width()
@@ -83,26 +97,25 @@ def add_count_label(ax, count, pct=False, as_pct=True, horiz=False,
             #  xmax = ax.get_xlim()[1]
             #  x_pos = lambda p, max_height: _label_offset(p) + .02 * xmax
 
-    def _ypos(patch, max_height):
+    def _ypos(patch):
         if orient == 'v':
-            return _patch_size(patch) / ymax
+            ypos = _patch_size(patch) / y_height
+
+            if loc == 'above' or loc == 'base':
+                ypos = ypos + offset
+            else:
+                ypos = ypos - offset
+            return ypos
             #  return _patch_size(patch) + .01 * max_height
         else:
             return patch.get_y() + 3 * patch.get_height() / 4.0
 
-    max_height = 0
-
-    for p in ax.patches:
-        # height = p.get_height()
-        height = _patch_size(p)
-        if height > max_height:
-            max_height = height
-
-    for p in ax.patches:
-        # height = p.get_height()
-        height = _patch_size(p)
-        if np.isnan(height):
-            height = 0
+    # sort by x position for palette cycling
+    patches = sorted(ax.patches, key=lambda p: p.get_x())
+    for i, p in enumerate(patches):
+        count = _patch_size(p)
+        if np.isnan(count):
+            count = 0
 
         if pct:
             if as_pct:
@@ -116,8 +129,12 @@ def add_count_label(ax, count, pct=False, as_pct=True, horiz=False,
             label = str(int(height))
             fontsize = fontsize
 
-        ax.text(_xpos(p, max_height), _ypos(p, max_height),
-                label,
-                ha=ha, va='bottom',
+        if palette is not None:
+            color = palette[i % len(palette)]
+
+        print(i, label)
+        ax.text(_xpos(p), _ypos(p),
+                label, color=color,
+                ha=ha, va=va,
                 fontsize=fontsize,
                 transform=ax.transAxes)
